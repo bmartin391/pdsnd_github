e530be5e6d5eb08c4e3178f4d8f127ae431b018e
Brandon <brandon.martin@toyota.com>
2021-07-28 11:43:57 -0500

    docs: Updated documentation on day filter

--- bikeshare_git_2.py
+++ bikeshare_git_2.py
@@ -558,7 +558,7 @@ def get_filters(city, month, day):
                 continue
             day = 'all'
             break
-
+        # This section asks the user if they would like to filter by day of week.
         elif filter_input == 'day':
             month = 'all'
             day = input(
